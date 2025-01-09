c9004ac5e6bb661d4488879ebb6586e973028603
Linda G Damasceno <50267260+lindagd@users.noreply.github.com>
2025-01-09 16:27:41 -0300

    improve all pages style

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,14 +3,16 @@ import '../index.css'
 
 const Sidebar = () => {
     return(
-        <div className="flex flex-wrap w-1/5 h-fit p-5 bg-pink/20 rounded-md text-white space-x-2">
-            <img src="https://via.placeholder.com/100" alt="User" className="size-20 rounded-full" />
+        <div className="flex flex-wrap items-center w-1/5 h-fit p-4 bg-pink/20 rounded-md shadow-md text-gray-800 space-x-2">
+            <img src="https://via.placeholder.com/100" alt="User" className="size-24 rounded-full border-2 border-pink/50 shadow-sm" />
             <div className="flex flex-col items-center">
-                <div className="text-xs text-pink">
-                    <div className="text-lg font-bold text-blue">Username</div>  
-                    <p>3 Pending Wishes</p>
-                    <p>5 Completed Wishes</p>
-                    <p>Total Spent: $500.00</p>
+                <div>
+                    <div className="text-lg font-bold text-pink">Username</div>  
+                    <div className="text-xs space-y-2">
+                       <p>3 Pending Wishes</p>
+                        <p>5 Completed Wishes</p>
+                        <p className="font-semibold text-blue">Total Spent: $500.00</p> 
+                    </div>
                 </div>
             </div>
         </div>
