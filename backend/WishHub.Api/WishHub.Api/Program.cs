08dfd255dcb95c38e4b26724c9d1658edb1a753a
Linda G Damasceno <50267260+lindagd@users.noreply.github.com>
2025-01-10 11:30:45 -0300

    add CORS config for connection

--- backend/WishHub.Api/WishHub.Api/Program.cs
+++ backend/WishHub.Api/WishHub.Api/Program.cs
@@ -13,6 +13,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+// CORS config
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +37,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowAll");
 
 app.MapControllers();
 
